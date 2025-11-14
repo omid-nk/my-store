b1e05ad090a30bb5a317d204cb2e076d6221742f
Omid Daliri <omiidnk02@gmail.com>
2025-11-14 22:15:57 +0330

    base

---
+++ src/main.js
@@ -0,0 +1,86 @@
+const products = [
+  {
+    id: 1,
+    title: "Laptop Asus VivoBook 14",
+    price: 2900,
+    cover: "./public/img/products/image01.jpg",
+    path: "./laptop1/"
+  },
+  {
+    id: 2,
+    title: "Laptop Asus ZenBook 13",
+    price: 3200,
+    cover: "./public/img/products/image02.jpg",
+    path: "./laptop2/"
+  },
+  {
+    id: 3,
+    title: "Laptop Asus ROG Strix 15",
+    price: 4500,
+    cover: "./public/img/products/image03.jpg",
+    path: "./laptop3/"
+  },
+  {
+    id: 4,
+    title: "Laptop Asus TUF Gaming 16",
+    price: 4100,
+    cover: "./public/img/products/image04.jpg",
+    path: "./laptop4/"
+  },
+  {
+    id: 5,
+    title: "Laptop Asus ExpertBook 14",
+    price: 3000,
+    cover: "./public/img/products/image05.jpg",
+    path: "./laptop5/"
+  },
+  {
+    id: 6,
+    title: "Laptop Asus ZenBook Flip 14",
+    price: 3400,
+    cover: "./public/img/products/image06.jpg",
+    path: "./laptop6/"
+  },
+  {
+    id: 7,
+    title: "Laptop Asus ROG Flow X13",
+    price: 4700,
+    cover: "./public/img/products/image07.jpg",
+    path: "./laptop7/"
+  },
+  {
+    id: 8,
+    title: "Laptop Asus TUF Dash 15",
+    price: 4200,
+    cover: "./public/img/products/image08.jpg",
+    path: "./laptop8/"
+  },
+  {
+    id: 9,
+    title: "Laptop Asus VivoBook S14",
+    price: 3100,
+    cover: "./public/img/products/image09.jpg",
+    path: "./laptop9/"
+  },
+  {
+    id: 10,
+    title: "Laptop Asus ZenBook 14 OLED",
+    price: 3600,
+    cover: "./public/img/products/image10.jpg",
+    path: "./laptop10/"
+  },
+  {
+    id: 11,
+    title: "Laptop Asus ROG Zephyrus G14",
+    price: 5000,
+    cover: "./public/img/products/image11.jpg",
+    path: "./laptop11/"
+  },
+  {
+    id: 12,
+    title: "Laptop Asus ExpertBook B9",
+    price: 3300,
+    cover: "./public/img/products/image12.jpg",
+    path: "./laptop12/"
+  }
+]
